0647b1d7a863058a77059533f95cb22fa128bde9
Park <ggomssngma@bible.ac.kr>
2018-11-29 23:51:54 +0900

    Appvue컴포넌트불러오고 파일 임포트

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+      <TodoHeader></TodoHeader>
+      <TodoInput></TodoInput>
+      <TodoList></TodoList>
+      <TodoFooter></TodoFooter>
+  </div>
 </template>
 
 
 <script>
+   import TodoHeader from './components/TodoHeader.vue'
+   import TodoFooter from './components/TodoFooter.vue'
+   import TodoInput from './components/TodoInput.vue'
+   import TodoList from './components/TodoList.vue'
     export default{
 
     }
