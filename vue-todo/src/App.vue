48b8e97043af9f66c1b2e32e7b65c639528e7abd
Park <ggomssngma@bible.ac.kr>
2018-11-30 00:27:06 +0900

    수정해서완료 기본틀 구조

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,30 +1,28 @@
 <template>
   <div id="app">
-      <TodoHeader></TodoHeader>
-      <TodoInput></TodoInput>
-      <TodoList></TodoList>
-      <TodoFooter></TodoFooter>
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
-
 <script>
-   import TodoHeader from './components/TodoHeader.vue';
-   import TodoInput from './components/TodoInput.vue';
-   import TodoList from './components/TodoList.vue';
-   import TodoFooter from './components/TodoFooter.vue';
-   
-   
-    export default{
-        componemts:{
-          'TodoHeader':TodoHeader,
-          'TodoInput':TodoInput,
-          'TodoList':TodoList,
-          'TodoFooter': TodoFooter
-        }
-    }
-
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+export default {
+ 
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
 </script>
-<style>
 
+<style>
+ 
 </style>
