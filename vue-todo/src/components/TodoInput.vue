80ba5f82cdf6a90afd12ea1387b9e2159713cc66
Park <ggomssngma@bible.ac.kr>
2018-12-03 00:52:46 +0900

    Input 값 예외처리

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -17,10 +17,20 @@
 
         methods:{
             addTodo(){
+                if(this.newTodoItem !==""){
+                    var value=this.newTodoItem && this.newTodoItem.trim();
+                    localStorage.setItem(value,value);
+                    this.clearInput();
+                }
+            },
+            clearInput(){
+                this.newTodoItem='';
+            }
                 //console.log(this.newTodoItem);
+                //localStorage.setItem(this.newTodoItem,this.newTodoItem);
             }
         }
-    }
+    
 </script>
 
 <style>
