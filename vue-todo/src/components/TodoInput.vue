733b8f3d309ff3022565709122328993b3bb9a15
Park <ggomssngma@bible.ac.kr>
2018-12-11 22:48:45 +0900

    경고창까지 완성

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -4,36 +4,49 @@
      <span class="addContainer" v-on:click="addTodo">
          <i class="addBtn fas fa-plus" aria-hidden="true"></i>
      </span>   
+
+     <modal v-if="showModal" @close="showModal=false">
+         <h3 slot="header">경고</h3>
+         <span slot="footer" @click="showModal=false">
+             할일을 입력하세요.
+             <i class="closeModalBtn fas fa-times" aria-hidden="true" ></i>
      <!-- <button v-on:click="addTodo">ADD</button> -->
-        
+         </span>
+     </modal>
     </div>
 </template>
 
 <script>
-
-    export default{
-        data(){
-            return{
-                newTodoItem:''
-            }
-        },
-
-        methods:{
-            addTodo(){
-                if(this.newTodoItem !==""){
-                    var value=this.newTodoItem && this.newTodoItem.trim();
-                    this.$emit('addTodo',value);
-                    //localStorage.setItem(value,value);
-                    this.clearInput();
-                }
-            },
-            clearInput(){
-                this.newTodoItem='';
-            }
+import Modal from './common/Modal.vue'
+export default {
+  data() {
+    return {
+      newTodoItem: '',
+      showModal: false
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+				this.$emit('addTodo', value)
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  },
+  components: {
+    Modal: Modal
+  }
+}
                 //console.log(this.newTodoItem);
                 //localStorage.setItem(this.newTodoItem,this.newTodoItem);
-            }
-        }
+            
+        
     
 </script>
 
