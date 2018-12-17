7948aa4c8f976460319d9161ba43222ff236d406
Park <ggomssngma@bible.ac.kr>
2018-12-18 00:04:19 +0900

    Table,th,tr,td로 그리는연습

---
+++ Vue-photo/myphoto/src/components/Body.vue
@@ -0,0 +1,21 @@
+<template>
+    <table border=1px weight="100%" height="100%" width="100%">
+        <tr>
+            <td colspan="2">1</td>
+        </tr>
+
+        <tr>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+
+    </table>
+</template>
+
+<script>
+    
+</script>
+
+<style>
+
+</style>
