d643f211f6c88d0037c9ce14b2ae6d457e49368c
Park <ggomssngma@bible.ac.kr>
2018-12-18 00:50:05 +0900

    Header 테이블수정

---
+++ Vue-photo/myphoto/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <table border=1px weight="100%" height="100%" width="100%">
+            <tr>
+                    <td>1</td>
+                    <td>1</td>
+            </tr>
+    </table>
+
+
+</template>
+
+<script>
+    
+</script>
+
+<style>
+
+</style>
