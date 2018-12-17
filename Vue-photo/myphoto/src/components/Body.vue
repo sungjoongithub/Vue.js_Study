86b2fa5e3efbc122989d6919171b8054ed1d6af2
Park <ggomssngma@bible.ac.kr>
2018-12-18 00:15:04 +0900

    table,th,td,tr을사용해서 구조짜기

--- Vue-photo/myphoto/src/components/Body.vue
+++ Vue-photo/myphoto/src/components/Body.vue
@@ -1,14 +1,24 @@
 <template>
     <table border=1px weight="100%" height="100%" width="100%">
         <tr>
-            <td colspan="2">1</td>
+            <td colspan="3">Hello1</td>
         </tr>
 
         <tr>
-            <td>2</td>
-            <td>3</td>
+            <td rowspan="2">Hello2</td>
+            <td>Hello3</td>
+            <td rowspan="2">Hello5</td>
         </tr>
 
+        <tr>
+            <td>Hello4</td>
+            
+        </tr>
+
+
+        <tr>
+            <td colspan="3">Hello6</td>
+        </tr>
     </table>
 </template>
 
