de0747c8393eb010e55272aeed8fb749b1371c58
Park <ggomssngma@bible.ac.kr>
2018-12-20 23:46:00 +0900

    메뉴추가

--- Vue-photo/myphoto/src/components/Header.vue
+++ Vue-photo/myphoto/src/components/Header.vue
@@ -10,6 +10,7 @@
           <li><a href="#news">News</a></li>
           <li><a href="#contact">Contact</a></li>
           <li><a href="#about">About</a></li>
+          <li><a href="#about">Select</a></li>
         </ul>
       </nav>
     </header>
